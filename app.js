e3c806dcdebf1bae95b77f72d8305a803ca1ea62
smilingkite <mail@katinkahesselink.net>
2018-04-10 10:33:02 +0200

    getting started

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var tl = new TimelineMax();
 
-tl.to( '.square-1', 2, { x: 150, ease:Sine.easeInOut, repeat: 5, yoyo: true }, 0 );
-tl.to( '.square-2', 1, { x: 150, ease:Sine.easeInOut, repeat: 11, yoyo: true }, 0 );
-tl.to( '.square', 0.5, { scale: 1.2, ease:Expo.easeOut, repeat: 7, yoyo: true, repeatDelay: 1 }, 0 );
+TweenMax.set('.twee',{autoAlpha:0});
+tl.to(TweenMax.to('.twee', 2, {autoAlpha:1}));
+tl.to( '.drie', 3, { x: 150, ease:Sine.easeInOut, repeat: 5, yoyo: true }, 0 );
+// tl.to( '.square-2', 1, { x: 150, ease:Sine.easeInOut, repeat: 11, yoyo: true }, 0 );
+// tl.to( '.square', 0.5, { scale: 1.2, ease:Expo.easeOut, repeat: 7, yoyo: true, repeatDelay: 1 }, 0 );

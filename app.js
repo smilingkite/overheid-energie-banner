ae7578eb8b0442375de062362ae03393ad6eb487
smilingkite <mail@katinkahesselink.net>
2018-04-10 11:46:24 +0200

    WIP layer 3

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var tl = new TimelineMax();
 
 TweenMax.set('.twee',{autoAlpha:0});
-tl.to(TweenMax.to('.twee', 2, {autoAlpha:1}));
-tl.to( '.drie', 3, { x: 150, ease:Sine.easeInOut, repeat: 5, yoyo: true }, 0 );
+TweenMax.set('.drie',{autoAlpha:0});
+// tl.to(TweenMax.to('.twee', 2, {autoAlpha:1}));
+// tl.to(TweenMax.to('.drie', 4, {autoAlpha:1}));
+tl.to(TweenLite.to('.drie',3, {left:"0px", autoAlpha:1, ease:Back.easeOut, delay: 3}));
 // tl.to( '.square-2', 1, { x: 150, ease:Sine.easeInOut, repeat: 11, yoyo: true }, 0 );
 // tl.to( '.square', 0.5, { scale: 1.2, ease:Expo.easeOut, repeat: 7, yoyo: true, repeatDelay: 1 }, 0 );

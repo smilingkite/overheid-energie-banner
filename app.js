3251eb3286ef8879f678f672c6dfce76dabff426
smilingkite <mail@katinkahesselink.net>
2018-04-10 12:30:01 +0200

    overlap animations

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var tl = new TimelineMax();
 
 TweenMax.set('.twee',{autoAlpha:0});
 TweenMax.set('.drie',{autoAlpha:0});
-tl.to('.twee', 2, {autoAlpha:1});
-tl.to('.drie', 2, {x: 310, autoAlpha:1, ease:Back.easeOut});
+tl.to('.twee', 1, {autoAlpha:1, });
+tl.to('.drie', 2, {x: 310, autoAlpha:1, ease:Back.easeOut}, "-=0.8");
 // tl.to( '.square-2', 1, { x: 150, ease:Sine.easeInOut, repeat: 11, yoyo: true }, 0 );
 // tl.to( '.square', 0.5, { scale: 1.2, ease:Expo.easeOut, repeat: 7, yoyo: true, repeatDelay: 1 }, 0 );

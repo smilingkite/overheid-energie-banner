9fdf7713c4ce7216059f2f344e9422bada31e11f
smilingkite <mail@katinkahesselink.net>
2018-04-10 13:02:07 +0200

    WIP dog talks

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ var tl = new TimelineMax();
 
 TweenMax.set('.twee',{autoAlpha:0});
 TweenMax.set('.drie',{autoAlpha:0});
+TweenMax.set('.vier',{autoAlpha:0});
 tl.to('.twee', 1, {autoAlpha:1, });
 tl.to('.drie', 2, {x: 310, autoAlpha:1, ease:Back.easeOut}, "-=0.8");
 tl
   .to('.drie', 0.5, {rotation:5, 
     ease:Power1.easeInOut, repeat:1, yoyo:true, repeatDelay:0.5}, 0.5)
-  .to('.drie', 0.25, {rotation:0},)
+  .to('.drie', 0.25, {rotation:0},);
+tl.to('.vier', 1, {autoAlpha:1, });
+tl.to('.vijf', 2, {x: -310, y: -10, autoAlpha:1, ease:Back.easeOut}, "-=0.8");
+tl
+  .to('.vijf', 0.5, {rotation:5, 
+    ease:Power1.easeInOut, repeat:1, yoyo:true, repeatDelay:0.5}, 0.5)
+  // .to('.vijf', 0.25, {rotation:0},);

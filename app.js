a0e63e918a57764a6081356d29d2b8762eed1c88
smilingkite <mail@katinkahesselink.net>
2018-04-11 16:12:49 +0200

    Tochdierenasiel tekst as html text

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ TweenMax.set('.vijfa',{autoAlpha:0});
 TweenMax.set('.boodschap',{autoAlpha:0});
 TweenMax.set('.eind',{autoAlpha:0});
 
-tl.to('.vier', 1, {autoAlpha:1}, "-=1");
+tl.to('.vier', 1, {autoAlpha:1}, "-=0.5");
 tl.to('.drie', 3, {x: -310, y: -5, autoAlpha:1, ease:Back.easeOut}, "-=1");
 tl.to('.vijfa', 2, {autoAlpha:1, }).delay(1);
 tl.to('.vijf', 2, {x: 310, autoAlpha:1, ease:Back.easeOut}, "-=1");

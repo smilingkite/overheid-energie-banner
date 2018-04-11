71c7e0943dd9d9829d5d727e8012cc85dd11e0ff
smilingkite <mail@katinkahesselink.net>
2018-04-11 17:01:54 +0200

    timing nuances

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ TweenMax.set('.boodschap',{autoAlpha:0});
 TweenMax.set('.eind',{autoAlpha:0});
 
 tl.to('.drie', 2, {x: -310, y: -5, ease:Back.easeOut});
-tl.to('.vijfa', 2, {autoAlpha:1, }, "+=1.5");
-tl.to('.vijf', 2, {x: 310, autoAlpha:1, ease:Back.easeOut}, "-=2");
+tl.to('.vijfa', 1, {autoAlpha:1, });
+tl.to('.vijf', 2, {x: 310, autoAlpha:1, ease:Back.easeOut}, "-=0.5");
 tl
   .to('.vijf', 0.5, {rotation:5, 
     ease:Power1.easeInOut, repeat:1, yoyo:true, repeatDelay:0.5}, "-=1")
   .to('.vijf', 0.25, {rotation:0}, "-=0.3");
-tl.to('.boodschap',1, {autoAlpha:1});
+tl.to('.boodschap',1, {autoAlpha:1}, "+=1");
 tl.to('.moves',3, {autoAlpha:1, y:-47});
 tl.to('.groen.moves',2, {autoAlpha:1, y:-90}, "-=1.5");
 tl.to('.tekst',5, {zIndex:70}, "-=5");
